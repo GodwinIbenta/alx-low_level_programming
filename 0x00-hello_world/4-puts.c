c099dc93ad59a66743a313cce9589c3120e3af6b
GodwinIbenta <ibentagodwin@gmail.com>
2022-10-19 14:56:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
- *
- *
  * Description: A C program that prints with put function
- *
- *
  *
  * Return: Always 0 (Sucess)
  *
@@ -15,9 +10,6 @@
 
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
